e7cf3912993983412ca4d482dae46c3fa75e5748
thabrkci <alpacinotaha@gmail.com>
2024-01-06 12:28:32 +0300

    Commitlistarray

--- Program.cs
+++ Program.cs
@@ -1,34 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
-Console.WriteLine("ArrayList");
-ArrayList liste = new ArrayList();
-//liste.Add("Taha");
-//liste.Add(2);
-//liste.Add('A');
-//liste.Add(true);
-//Console.WriteLine(liste[0]);
-//foreach( var item in liste)
-//Console.WriteLine(item);
-//Addrange(Bir koleksiyona başka bir koleksiyon eklemek için kullanılır)
-Console.WriteLine("***AddRange");
-//string[] renkler = {"kırmızı","sarı","yeşil","mavi"};
-//foreach( var item in renkler)
-//Console.WriteLine(item);
-List<int> sayılar =new List<int>(){1,3,5,7,8,9,99};
-//foreach( var item in sayılar)
-//Console.WriteLine(item);
-liste.AddRange(sayılar);
-foreach( var item in liste)
-Console.WriteLine(item);
-//Sort
-Console.WriteLine("***SORT***");
-liste.Sort();
-foreach( var item in liste)
-Console.WriteLine(item);
-Console.WriteLine("***REVERSE***");
-liste.Reverse();
-foreach( var item in liste)
-Console.WriteLine(item);
-//Binary Search
-Console.WriteLine("***BİNARY SEARCH");
-Console.WriteLine(liste.BinarySearch(9));
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("ArrayList");
+        ArrayList list = new ArrayList();
+        
+        // Commented out previous code
+        // list.Add("Taha");
+        // list.Add(2);
+        // list.Add('A');
+        // list.Add(true);
+        // Console.WriteLine(list[0]);
+        // foreach (var item in list)
+        //     Console.WriteLine(item);
+
+        // AddRange: Adds elements from a collection to the ArrayList
+        Console.WriteLine("***AddRange");
+
+        // Commented out previous code
+        // string[] colors = {"red", "yellow", "green", "blue"};
+        // foreach (var item in colors)
+        //     Console.WriteLine(item);
+
+        List<int> numbers = new List<int>() {1, 3, 5, 7, 8, 9, 99};
+        // foreach (var item in numbers)
+        //     Console.WriteLine(item);
+
+        list.AddRange(numbers);
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        // Sort: Sorts the elements in the entire ArrayList
+        Console.WriteLine("***SORT");
+        list.Sort();
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        // Reverse: Reverses the order of the elements in the entire ArrayList
+        Console.WriteLine("***REVERSE");
+        list.Reverse();
+        foreach (var item in list)
+            Console.WriteLine(item);
+
+        // BinarySearch: Searches for the specified object and returns the zero-based index of the first occurrence within the entire ArrayList
+        Console.WriteLine("***BINARY SEARCH");
+        Console.WriteLine(list.BinarySearch(9));
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+Herkese selam,
+Listarray ile eleman ekleme eleman çıkarma yazdırma ve farklı biçimlerde yazdırma ile ilgili birkaç örnek göstermeye çalıştım.
+Gerekli açıklamaları yorum satırlarında bulabilirsiniz.
+Değerli vaktinizi harcadığınız için çok teşekkür ederim.
+--------------------------------------------------------
+Hello everyone,
+I tried to show a few examples about adding elements, removing elements, printing and printing in different formats with Listarray.
+You can find the necessary explanations in the comment lines.
+Thank you very much for spending your valuable time.
